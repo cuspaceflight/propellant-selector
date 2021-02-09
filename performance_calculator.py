d0a10fcab49ec7318b76cad3d54ab8dccd8185e1
Tim Clifford <tclifford@protonmail.com>
2021-02-09 16:24:40 +0000

    Make Sam's secret sauce hardcoded values more explicit

--- performance_calculator.py
+++ performance_calculator.py
@@ -1,6 +1,6 @@
 from propellant import Propellant, Propellant_Mix, Pressurant
 from nist_reader import propellant_data
-from numpy import pi
+from math import pi, ceil
 
 
 class Material(object):
@@ -11,26 +11,24 @@ class Material(object):
 
 
 class Vehicle(object):
-    def __init__(self, propellant_mix, material, pressurant, diameter):
+    def __init__(self, propellant_mix, material, pressurant, diameter, ullage):
         self.propellant_mix = propellant_mix
         self.material = material
         self.pressurant = pressurant
         self.radius = diameter/2
-
-        # Tank volume that must be gas - change if current thinking changes,
-        # but a good rule of thumb
-        self.ullage = 0.1
-
+        self.ullage = ullage
         self.temperature = self.propellant_mix.temperature
 
-    def calculate_minimum_tank_thickness(self):
-        pressure = self.propellant_mix.pressure * 100000
+    def set_tank_thickness(self, safety_factor, resolution):
+        pressure = self.propellant_mix.pressure * 1e5
+        min_thickness = pressure * self.radius / self.material.yield_strength
+        self.wall_thickness = \
+                ceil(safety_factor * min_thickness / resolution) * resolution
 
-        thickness = pressure * self.radius / self.material.yield_strength
-
-        self.wall_thickness = thickness
-
-    def calculate_representative_pressurant_data(self):
+    def set_representative_pressurant_data(self,
+                pressurant_storage_pressure,
+                pressurant_tank_safety_factor,
+                pressurant_plumbing_mass_factor):
         """
         Representative Pressurant Mass is the mass of pressurant gas needed to
         fill a Representative Length tank to full pressure, accounting for
@@ -38,16 +36,7 @@ class Vehicle(object):
 
         Representative Pressurant System Mass is the mass of spherical tanks + \
         pressurant needed to achieve this
-
-        Warning: includes some arbitrary hard-coded values for pressurant storage
         """
-        # lower end of paintball tank pressure
-        pressurant_storage_pressure = 210 # bar
-        pressurant_tank_safety_factor = 2
-
-        # additional increase in mass of system to account for valves and
-        # plumbing - completely arbitrary
-        pressurant_plumbing_mass_factor = 5
 
         area = (self.radius**2 * pi) - \
                (2 * pi * self.radius * self.wall_thickness)
@@ -100,7 +89,7 @@ class Vehicle(object):
         self.rep_pressurant_mass = representative_pressurant_mass
         self.rep_pressurant_system_mass = pressurant_system_mass
 
-    def calculate_representative_scales(self):
+    def set_representative_scales(self, bulkhead_mass_factor):
         """
         Representative Length is the total length of tank required for 1kg of
         fuel and equivalent oxidiser
@@ -116,8 +105,6 @@ class Vehicle(object):
 
         Note that this does not include bulkheads etc - potential future feature?
         """
-        bulkhead_mass_factor = 1.25
-
         effective_fuel_density = \
             (self.ullage     * self.propellant_mix.fuel.vapor_density(self.temperature)) + \
             ((1-self.ullage) * self.propellant_mix.fuel.liquid_density(self.temperature))
@@ -156,7 +143,7 @@ class Vehicle(object):
         self.rep_mass = representative_mass
         self.rep_system_mass = representative_system_mass
 
-    def calculate_representative_engine_data(self):
+    def set_representative_engine_data(self):
         """
         Representative thrust - assume a max flow rate of 0.1kg/s for oxidiser
         Representative TWR - representative Thrust divided by (mass of )
@@ -168,11 +155,8 @@ class Vehicle(object):
         mass_flow = oxidiser_mass_flow + fuel_mass_flow
 
         representative_thrust = self.propellant_mix.ISP_sea_level * 9.81 * mass_flow
-
         representative_mass = self.rep_system_mass + self.rep_pressurant_system_mass
-
         representative_TWR = representative_thrust / representative_mass
-
         representative_impulse = representative_thrust * burn_time
 
         self.rep_thrust = representative_thrust
@@ -219,20 +203,47 @@ class Vehicle(object):
             if len(i) > 0 and i[0] == "|" else i for i in outstr.split("\n"))
 
 
-aluminium = Material("aluminium", 170*(10**6), 2700)
+if __name__ == "__main__":
 
-pressurants = [
-    Pressurant("helium", 0.004, 1.66),
-    Pressurant("nitrogen", 0.028, 1.40),
-    Pressurant("carbon-dioxide", 0.044, 1.29),
-]
+    aluminium = Material("aluminium", 170*(10**6), 2700)
+    pressurants = {
+        "helium":         Pressurant("helium",          0.004, 1.66),
+        "nitrogen":       Pressurant("nitrogen",        0.028, 1.40),
+        "carbon-dioxide": Pressurant("carbon-dioxide",  0.044, 1.29),
+    }
 
-ethane_nitrous = Propellant_Mix("ammonia", "nitrous", 250, 17)
-test_tank = Vehicle(ethane_nitrous, aluminium, pressurants[2], 0.1524)
-test_tank.calculate_minimum_tank_thickness()
-test_tank.wall_thickness = 0.004
-test_tank.calculate_representative_scales()
-test_tank.calculate_representative_pressurant_data()
-test_tank.calculate_representative_engine_data()
+    ethane_nitrous = Propellant_Mix("ammonia", "nitrous", 250, 17)
 
-print(test_tank)
+    test_tank = Vehicle(
+        propellant_mix = ethane_nitrous,
+        material       = aluminium,
+        pressurant     = pressurants["carbon-dioxide"],
+        diameter       = 0.1524,
+        # SR: "Tank volume that must be gas - change if current thinking
+        # changes, but a good rule of thumb"
+        ullage         = 0.1,
+    )
+
+    # TC: "Not sure why this safety factor is so high but this makes it the
+    # same as SR's original hard-coded value"
+    test_tank.set_tank_thickness(
+        safety_factor = 5,
+        resolution = 0.001, # Rounded up to a multiple of this value
+    )
+
+    test_tank.set_representative_scales(
+        bulkhead_mass_factor = 1.25
+    )
+
+    test_tank.set_representative_pressurant_data(
+        # lower end of paintball tank pressure
+        pressurant_storage_pressure = 210, # bar
+        pressurant_tank_safety_factor = 2,
+
+        # additional increase in mass of system to account for valves and
+        # plumbing - completely arbitrary
+        pressurant_plumbing_mass_factor = 5,
+    )
+    test_tank.set_representative_engine_data()
+
+    print(test_tank)
